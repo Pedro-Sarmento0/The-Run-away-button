b64fd647aee8754fc2f426079535e27cad7eff88
Pedro Sarmento TI01TA <pedro_sarmento0@hotmail.com>
2026-06-19 19:23:25 -0300

    completion

--- secret/script-secret.js
+++ secret/script-secret.js
@@ -4,5 +4,14 @@ function doRunaway() {
     btnReturnSecret.style.position = 'absolute';
     const limitX = window.innerWidth - btnReturnSecret.offsetWidth;
     const limitY = window.innerHeight - btnReturnSecret.offsetHeight;
-    const randomX = Math.max(10, Math.floor(Math.random() * (limitX - 10)))
+    const randomX = Math.max(10, Math.floor(Math.random() * (limitX - 10)));
+    const randomY = Math.max(10, Math.floor(Math.random() * (limitY - 10)));
+    btnReturnSecret.style.left = randomX + 'px';
+    btnReturnSecret.style.top = randomY + 'px';
 }
+
+btnReturnSecret.addEventListener('mouseover', doRunaway);
+
+btnReturnSecret.addEventListener('click', () => {
+    window.location.href = 'index.html';
+});
